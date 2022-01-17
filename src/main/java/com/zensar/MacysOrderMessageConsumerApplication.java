e8396aa0a868466dbe6f37452f98d6ae4352104c
Nawaz <vmnawaz@gmail.com>
2022-01-17 23:27:19 +0530

    added test case

--- src/main/java/com/zensar/MacysOrderMessageConsumerApplication.java
+++ src/main/java/com/zensar/MacysOrderMessageConsumerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 
+
 @SpringBootApplication
 public class MacysOrderMessageConsumerApplication {
 
@@ -15,27 +16,6 @@ public class MacysOrderMessageConsumerApplication {
 		
 		System.out.println("Inside consumer");
 	}
-	
-//	@Bean
-//	public Docket getDocket() {
-//		return new Docket(DocumentationType.SWAGGER_2)
-//				.apiInfo(getApiInfo())
-//				.select()
-//				.apis(RequestHandlerSelectors.basePackage("com.zensar"))
-//				.paths(PathSelectors.any())
-//				.build();
-//	}
-//	
-//	private ApiInfo getApiInfo() {
-//		return new ApiInfo(
-//			      "MACY'S Message Producer REST Api documention",
-//			      "MACY'S Message Producer REST api doc released by Zensar",
-//			      "1.0",
-//			      "http://zensar.com",
-//			      new Contact("MACY'S Message Producer","http://xensar.com","contact@zensar.com"),
-//			      "HTTP",
-//			      "GPA",
-//			      new ArrayList<VendorExtension>());
-//	}
+
 
 }
--- src/test/java/com/zensar/controller/OrderMessageConsumerControllerTest.java
+++ src/test/java/com/zensar/controller/OrderMessageConsumerControllerTest.java
@@ -1,5 +1,41 @@
 package com.zensar.controller;
 
-public class OrderMessageConsumerControllerTest {
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import com.zensar.services.OrderMessageConsumerService;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrderMessageConsumerControllerTest {
+	
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	final void testRetrieveJsonMessages() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/consume/json").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+	}
+	
+	@Test
+	final void testRetrieveXmlMessages() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/consume/xml").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+	}
+	
+
+	
+	
 }
